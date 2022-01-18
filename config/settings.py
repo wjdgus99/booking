a206a59777aa7494ff1dac2facaff64acd8835d8
origin <ljhyun715@gmail.com>
2022-01-18 16:54:33 +0900

    get token

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        "api_key": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header"
+        },
+    },
+    "LOGIN_URL": "/admin/login",
+    "LOGOUT_URL": "/admin/logout/"
+}
