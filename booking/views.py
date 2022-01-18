9fc9a7dcb19e7b33a1a6c57e41ffb9c0daec78f1
origin <ljhyun715@gmail.com>
2022-01-18 16:11:40 +0900

    drf_yasg (API doc)

---
+++ booking/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+
+from .models import Booking
+from .serializers import BookingSerializer
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class BookingList(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = (TokenAuthentication,)
+    permissions_classes = (IsAuthenticated,)
+
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
